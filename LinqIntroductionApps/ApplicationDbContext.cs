70b971b7146525710cb6f527a0fc07eab2852324
mdshohagmia <shohaghassan14@hotmail.com>
2019-03-19 22:57:21 +0600

    Update

---
+++ LinqIntroductionApps/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace LinqIntroductionApps
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DataSource")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ LinqIntroductionApps/Migrations/201903191654048_Initial.cs
@@ -0,0 +1,27 @@
+namespace LinqIntroductionApps.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        FullName = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.StudentId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
